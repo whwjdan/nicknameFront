243063f807c3715a550e21652c609ea19cd9cbda
whwjdan <whwjdan@naver.com>
2023-11-14 15:09:31 +0900

    닉네임 리스트 페이징 완료

--- src/views/NicknameList.vue
+++ src/views/NicknameList.vue
@@ -3,27 +3,65 @@
     <v-container>
       <v-row>
         <v-col
-            v-for="n in 16"
-            :key="n"
+            v-for="(item, index) in visiblePages"
+            :key="index"
             cols="3"
-            md="4"
+            md="3"
+            xl="3"
         >
-          <v-item v-slot="{ isSelected, selectedClass, toggle }">
-            <v-card
-                :class="['d-flex align-center', selectedClass]"
-                dark
-                height="200"
-                @click="toggle"
-            >
-              <div
-                  class="text-h3 flex-grow-1 text-center"
-              >
-                {{ isSelected ? 'Selected' : 'Click Me!' }}
-              </div>
-            </v-card>
-          </v-item>
+          <v-card class="mt-4 mb-4 mx-auto" max-width="200">
+            <v-img class="white--text align-end" height="150px"
+                   :lazy-src="item.image">
+              <v-card-title>{{item.name.title}}</v-card-title>
+            </v-img>
+            <v-card-text class="text--primary">
+              <div>{{item.name.title}}</div>
+            </v-card-text>
+          </v-card>
         </v-col>
       </v-row>
     </v-container>
   </v-item-group>
+  <v-pagination
+      v-model="page"
+      :length="Math.ceil(pages.length/perPage)"
+  ></v-pagination>
 </template>
+
+<script>
+
+import axios from "axios";
+
+var url = "https://randomuser.me/api/?results=20"
+
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      page: 1,
+      perPage: 8,
+      pages: []
+    }
+  },
+  created: function () {
+    this.getUsers()
+  },
+  methods:{
+    getUsers(){
+      axios.get(url)
+          .then(res =>
+  { this.pages = res.data.results})
+          .catch(error => {
+            console.log(error)
+          })
+    }
+  },
+  computed: {
+    visiblePages () {
+      return this.pages.slice((this.page - 1)* this.perPage, this.page* this.perPage)
+    }
+  }
+}
+
+</script>
